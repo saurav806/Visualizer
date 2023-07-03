99d719a324f12f73bda9f5b053de6f247ba4b30d
Saurav Kumar <golukumar806@gmail.com>
2023-07-03 14:25:33 +0530

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let randomize_array = document.getElementById("randomize_array_btn");
 let sort_btn  = document.getElementById("sort_btn");
+let stop_btn = document.getElementById("stop_btn");
 let bars_container = document.getElementById("bars_container");
 let minRange = 1;
 let maxRange = 20;
-let numOfBars = 90;
+let numOfBars = 30;
 let unsorted_array = new Array(numOfBars);
 
 function randomNum(min,max) {
@@ -26,7 +27,7 @@ function renderBars(array){
     for( let i=0 ; i<array.length; i++) {
         let bar = document.createElement("div");
         bar.classList.add("bar");
-        bar.style.height = array[i] * 20 + "px";
+        bar.style.height = array[i] * 18 + "px";
         bars_container.appendChild(bar);
     }
 }
@@ -41,14 +42,20 @@ function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve,ms));
 }
 
+// let bool=false;
+
 async function bubbleSort(array){
+    
     let bars = document.getElementsByClassName("bar");
+    let lcolor;
+    
+    
     for(let i = 0; i< array.length; i++){
         for( let j=0; j<array.length-i-1; j++){
             if(array[j]>array[j+1]){
                 for( let k = 0; k<bars.length; k++){
                     if(k !== j && k !== j+1){
-                        bars[k].style.backgroundColor = 'orange';
+                        bars[k].style.backgroundColor = 'rgb(190,236,245)';
                     }
                 }
                 let temp = array[j];
@@ -56,21 +63,38 @@ async function bubbleSort(array){
                 array[j+1] = temp;
 
                 //styling 
-                bars[j].style.height = array[j] * 10 + "px";
-                bars[j].style.backgroundColor = "lightgreen";
                 // bars[j].innerText = array[j];
-                bars[j+1].style.height = array[j+1] * 10 + "px";
+                bars[j+1].style.height = array[j+1] * 18 + "px";
                 bars[j+1].style.backgroundColor = "lightgreen";
                 // bars[j+1].innerText = array[j];
-                await sleep(30);
+                
+                bars[j].style.height = array[j] * 18 + "px";
+                bars[j].style.backgroundColor = "pink";
+                await sleep(50);
             }
+            
         }
-        await sleep(30);
+        await sleep(50);
     }
     return array;
 }
+
+// function stop_function(bool){
+//     return false;
+// }
+
+// function stop_function() {
+//     clearInterval(timeValue);
+//  }
+                
 
 sort_btn.addEventListener("click", function() {
     let sorted_array = bubbleSort(unsorted_array);
     console.log(sorted_array);
 });
+
+// stop_btn.addEventListener("click", stop_function());
+
+// stop_btn.addEventListener("click",stop_function() {
+//     bool=stop_function(bool);
+// });
